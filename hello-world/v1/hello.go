7b4a258ef7f835b786a27abcc67caf0202854475
Sergio <sergiolpf@gmail.com>
2022-09-12 23:15:34 +1200

    a lot happened here, check the code for more info

--- hello-world/v1/hello.go
+++ hello-world/v1/hello.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefis = "Hello, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const englishWorldSufix = "World"
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		name = "World"
+		name = englishWorldSufix
 	}
-	return englishHelloPrefis + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
